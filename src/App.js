0a8d5722b14254d65ec898b073fda06664f6f5be
Emmanuel <manu_bassman@hotmail.com>
2023-11-06 10:33:26 +0100

    "añadir cambios"

---
+++ src/App.js
@@ -0,0 +1,36 @@
+
+import './App.css';
+import TrafficLight from "./TrafficLight";
+
+const trafficStates = {
+  red: {
+    duration: 4000,
+    backgroundColor: "red",
+    next: "green"
+  },
+  yellow: {
+    duration: 500,
+    backgroundColor: "yellow",
+    next: "red"
+  },
+  green: {
+    duration: 3000,
+    backgroundColor: "green",
+    next: "yellow"
+  }
+};
+
+function App() {
+  return (
+    <div className="wrapper">
+      <TrafficLight trafficStates={trafficStates} />
+    </div>
+  );
+}
+
+export default App;
+
+
+
+
+
